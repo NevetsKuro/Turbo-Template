368ac386ba2e13b69268bae482d3a698aa13b4cc
steven <Steven.fernandes@dripcapital.com>
2022-03-01 16:23:27 +0530

    Update index.tsx

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -4,8 +4,7 @@ export default function Web() {
   return (
     <div>
     {/* Trigger GA - flip value: true */}
-      <h1>Click to do nothing!</h1>
-      <Button />
+      <h1>Click to do nothing at all!</h1>
       <Button />
     </div>
   );
