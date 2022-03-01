8e5437a5a323054e73a16019d54f1173c8e95e58
Steven Fernandes <steven.fernandes@dripcapital.com>
2022-03-01 14:23:06 +0530

    trigger: both folders

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -3,6 +3,7 @@ import { Button } from "ui";
 export default function Docs() {
   return (
     <div>
+      {/* Trigger GA - flip value: true */}
       <h1>Turbo Docs (Button)</h1>
       <Button />
     </div>
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -3,6 +3,7 @@ import { Button } from "ui";
 export default function Web() {
   return (
     <div>
+    {/* Trigger GA - flip value: true */}
       <h1>Click to do nothing!</h1>
       <Button />
     </div>
