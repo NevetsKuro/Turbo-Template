f98d6953d8061c51c8ed18d0e071c28c691f81a9
Steven Fernandes <steven.fernandes@dripcapital.com>
2022-03-02 15:47:20 +0530

    chore: modified firebase

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -4,7 +4,7 @@ export default function Web() {
   return (
     <div>
       {/* Trigger GA - flip value: true */}
-      <h1>Click to do nothing!</h1>
+      <h1>Main Website</h1>
       <Button />
     </div>
   );
