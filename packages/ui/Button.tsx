ce41aea14b92ad73ad0c2239683387ee9a007992
Steven Fernandes <steven.fernandes@dripcapital.com>
2022-03-02 13:06:08 +0530

    chore: trigger workflow

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -4,7 +4,7 @@ export const Button = () => {
   const [count, setCount] = React.useState(0);
   return (
     <div>
-      <button onClick={() => {setCount(count+1);setMsg("Don't click me!!")}}>Be-Boop-Bump *</button>
+      <button onClick={() => {setCount(count+1);setMsg("Don't click me!!")}}>Be-Boop-Bump</button>
       <h1>{msg} x{count}</h1>
     </div>
   );
