e0dae4ac53b4beafc274442bc70eadfc2c040f57
steven <Steven.fernandes@dripcapital.com>
2022-03-17 15:44:38 +0530

    Update Button.tsx

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -3,8 +3,10 @@ import "./css/style.css"
 
 export const Button = ({callbackFn, title}:{callbackFn: React.EventHandler<any> ,title: string}) => {
   return (
-    <button className='primary-button' title={title} onClick={callbackFn}>
-      {title}
-    </button>
+    <>
+      <button className='primary-button' title={title} onClick={callbackFn}>
+        {title}
+      </button>
+    </>
   );
 };
